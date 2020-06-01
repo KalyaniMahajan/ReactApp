6531a8354ecc5c56a599c47b5f2fe95bda595b1a
Kalyani Mahajan <59738224+KalyaniMahajan@users.noreply.github.com>
2020-06-01 11:04:19 +0530

    create basic clock using JSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let name = <p>Harry Potter Clock</p>
 
-ReactDOM.render(
-  <h1>Hello Harry Potter</h1>,
-  document.getElementById('root')
-);
+setInterval(function(){
+	ReactDOM.render(
+		<h2>{name} The Current Time is - {new Date().toLocaleTimeString()}</h2>,
+	  document.getElementById('root')
+	);	
+}, 1200);
+
